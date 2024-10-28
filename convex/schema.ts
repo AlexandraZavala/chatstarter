43a2c3f62d3a7ea854a13439e9d65d80a693aa34
Alexandra Zavala <a20181040@pucp.edu.pe>
2024-10-27 21:42:10 -0500

    add wh

--- convex/schema.ts
+++ convex/schema.ts
@@ -2,6 +2,11 @@ import { defineSchema, defineTable } from "convex/server";
 import { v } from "convex/values"; //validate values 
 
 export default defineSchema({
+    users: defineTable({
+        image: v.string(),
+        username: v.string(),
+        clerkId: v.string(),
+    }).index("by_clerkId", ["clerkId"]), //for indexation
     messages: defineTable({
         sender: v.string(),
         content: v.string(),
